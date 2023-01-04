2f6850e490fd5bc48abd0744931cf04d601f772e
Fuad <fuadmiftah9@gmail.com>
2023-01-05 00:56:19 +0300

    first-commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended : true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/", function(req, res){
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+
+    var bmi = weight/ (height * height);
+    res.send("The result of bmi is " + bmi);
+});
+
+app.listen(3000, function(){
+    console.log("app is running on port 3000");
+});
